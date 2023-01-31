e0287a4cf91f2ced59baec2bf0bedaf1f73eea2b
Ted Janney <janneyt@oregonstate.edu>
2023-01-31 12:43:34 -0500

    Began adding doxygen documentation

--- smallsh.c
+++ smallsh.c
@@ -1,3 +1,6 @@
+/**
+ * # Imports #
+ */
 # include <stdio.h>
 # include <sys/types.h>
 # include <signal.h>
@@ -7,15 +10,59 @@
 # include <stdlib.h>
 # include <assert.h>
 
+/**
+ * # Macros #
+ */
+
+# define 
+
+
+/**
+ * # Code
+ */
+# Code #
+
+/**
+ * ## Error handling functions
+ */
 int child_error(char child_action[], char action_taken[]){
+	/**
+	 * \brief Produces an error because a child process has completed in a systematic way.
+	 *
+	 * @param child_action is a string of arbitrary size that represents *what* caused completion
+	 * @param action_taken is a string of arbtrary size that represents *what was the result* such as continuing or exiting.
+	 *
+	 * Writes to stderr.
+	 *
+	 * @return EXIT_SUCCESS if process successfully writes to stderr, EXIT_FAILURE if there was an error while writing
+	 */
 	fprintf(stderr, "Child process %s. %s.", child_action, action_taken);
 	return (EXIT_SUCCESS);
 }
 
+/**
+ * ## Utility functions 
+ */
+
 int int_to_string(int num, char* str, int size){
+	
+	/**
+	 * \brief Converts an integer to its strings representation
+	 *
+	 * @param num is the integer that needs to be converted to a string
+	 * @param str is the holder string that stores the integer representation
+	 * @param size is the maximum length possible to store in str, including \0
+	 * @return EXIT_SUCCESS if integer has been entirely converted to 
+	 * string, EXIT_FAILURE if integer cannot be convered to string
+	 *
+	 */
+
 	char temp[size];
-	sprintf(temp, "%d", num);
-	if(strlen(str) < strlen(temp)){
+	if(sprintf(temp, "%d", num) < 0){
+		perror("Could not convert integer to string");
+		return EXIT_FAILURE;
+	};
+	if(strlen(str) <= strlen(temp)){
 		return EXIT_FAILURE;
 	}
 	if(strcat(str, temp) == NULL){
@@ -24,6 +71,10 @@ int int_to_string(int num, char* str, int size){
 	};
 	return EXIT_SUCCESS;
 }
+
+/**
+ * ## Functions needed to meet specification requirements
+ */
 
 int check_for_child_background_processes(int status, int pid){
 	int  string_size = 100;
