599244b758f3ee356bfe490a3ed07f4c946d996b
Ted Janney <janneyt@oregonstate.edu>
2023-01-31 16:47:56 -0800

    ignore this

--- smallsh.c
+++ smallsh.c
@@ -6,13 +6,10 @@
 # include <string.h>
 # include <stdlib.h>
 # include <assert.h>
-
-int child_error(char child_action[], char action_taken[]){
-	fprintf(stderr, "Child process %s. %s.", child_action, action_taken);
-	return (EXIT_SUCCESS);
-}
+# include <stdint.h>
 
 int int_to_string(int num, char* str, int size){
+	// Converts an integer to its string representation
 	char temp[size];
 	sprintf(temp, "%d", num);
 	if(strlen(str) < strlen(temp)){
@@ -26,40 +23,33 @@ int int_to_string(int num, char* str, int size){
 }
 
 int check_for_child_background_processes(int status, int pid){
-	int  string_size = 100;
-	char child_action[string_size];
-	char action_taken[string_size];
-	memset(child_action, 0, sizeof(child_action));
-	memset(action_taken, 0, sizeof(action_taken));
-	assert(child_action == 0x0);
-	assert(action_taken == 0x0);
 
 	if (WIFEXITED(status)){
-		strcat(child_action,"done");
-		strcat(action_taken, "Exit value ");
-		if(int_to_string(WEXITSTATUS(status), action_taken, string_size) != EXIT_SUCCESS){
-			 return EXIT_FAILURE;
+		if(fprintf(stderr, "Child process %jd done. Exit status %d.\n", (intmax_t) pid, WEXITSTATUS(status)) < 0){
+			perror("Could not print");
+			return EXIT_FAILURE;
 		};
 
 	}
 	else if (WIFSIGNALED(status)){
-		strcat(child_action, "done");
-		strcat(action_taken, "Signaled ");
-		if(int_to_string(WTERMSIG(status), action_taken, string_size) != EXIT_SUCCESS){
+		if(fprintf(stderr, "Child process %jd done. Signaled %d.\n", (intmax_t) pid, WTERMSIG(status)) < 0){
+			perror("Could not print");
 			return EXIT_FAILURE;
 		};
-
+		
 	}
 	else if (WIFSTOPPED(status)) {
-		strcat(child_action, "stopped");		
-		strcat(action_taken,"Continuing");
 		if(kill(pid, SIGCONT) != 0){
 			perror("Kill didn't work");
 			return EXIT_FAILURE;
 		}
+		if(fprintf(stderr, "Child process %jd stopped. Continuing.\n", (intmax_t) pid) < 0){
+			perror("Could not print");
+			return EXIT_FAILURE;
+		};
 
 	}
-	return child_error(child_action, action_taken);
+	return EXIT_SUCCESS;
 }
 
 int main(){
